1e896cd5248091feaaeaa4b38726d58996dd4e53
Janis Stolzenwald <janis.stolzenwald@tngtech.com>
2021-08-25 09:33:46 +0200

    adds api

--- src/pages/Tab1.tsx
+++ src/pages/Tab1.tsx
@@ -2,7 +2,40 @@ import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/rea
 import ExploreContainer from '../components/ExploreContainer';
 import './Tab1.css';
 
+import React, { useState, useEffect } from 'react';
+import { API } from 'aws-amplify';
+import { listNotes } from '../graphql/queries';
+import { createNote as createNoteMutation, deleteNote as deleteNoteMutation } from '../graphql/mutations';
+
+const initialFormState = { name: '', description: '' }
+
 const Tab1: React.FC = () => {
+
+    const [notes, setNotes] = useState([]);
+    const [formData, setFormData] = useState(initialFormState);
+
+    useEffect(() => {
+        fetchNotes();
+    }, []);
+
+    async function fetchNotes() {
+        const apiData = await API.graphql({ query: listNotes });
+        setNotes(apiData.data.listNotes.items);
+    }
+
+    async function createNote() {
+        if (!formData.name || !formData.description) return;
+        await API.graphql({ query: createNoteMutation, variables: { input: formData } });
+        setNotes([ ...notes, formData ]);
+        setFormData(initialFormState);
+    }
+
+    async function deleteNote({ id }) {
+        const newNotesArray = notes.filter(note => note.id !== id);
+        setNotes(newNotesArray);
+        await API.graphql({ query: deleteNoteMutation, variables: { input: { id } }});
+    }
+
   return (
     <IonPage>
       <IonHeader>
@@ -16,6 +49,29 @@ const Tab1: React.FC = () => {
             <IonTitle size="large">Tab 1</IonTitle>
           </IonToolbar>
         </IonHeader>
+          <h1>My Notes App</h1>
+          <input
+              onChange={e => setFormData({ ...formData, 'name': e.target.value})}
+              placeholder="Note name"
+              value={formData.name}
+          />
+          <input
+              onChange={e => setFormData({ ...formData, 'description': e.target.value})}
+              placeholder="Note description"
+              value={formData.description}
+          />
+          <button onClick={createNote}>Create Note</button>
+          <div style={{marginBottom: 30}}>
+              {
+                  notes.map(note => (
+                      <div key={note.id || note.name}>
+                          <h2>{note.name}</h2>
+                          <p>{note.description}</p>
+                          <button onClick={() => deleteNote(note)}>Delete note</button>
+                      </div>
+                  ))
+              }
+          </div>
         <ExploreContainer name="Tab 1 page" />
       </IonContent>
     </IonPage>
